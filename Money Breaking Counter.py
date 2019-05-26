da5f8594140a75ef763279de3954c77bde73801b
KeeganBarbosa <50556001+KeeganBarbosa@users.noreply.github.com>
2019-05-26 11:45:30 -0400

    Add files via upload
    Massive update on previous coin counting algorithm. This one accurately counts the number of ways to pay x dollars from a collection of coins Coin_List. This answers Project Euler question 31.

--- Money Breaking Counter.py
+++ Money Breaking Counter.py
@@ -11,16 +11,36 @@ def how_to_split_a(x,CoinType):
     init=0
     if x == 0:
         return 1
+    if x<0:
+        return 0
+    if not CoinType:
+        return 0
     else:
-        for c in [c for c in CoinType if c<=x]:
-            init =init+ how_to_split_a(x-c,CoinType)
+        y=max(CoinType)
+        init = how_to_split_a(x,[c for c in CoinType if c<y])+ how_to_split_a(x-y,CoinType)
     return init
 
 
 """
 This recursive code counts how many ways to give x change with CoinType coint types
 
-Note, order matters for this code. That is, paying with a quarter and a dime is different from paying with a dime and a quarter.
+The technique is to utilize recursion. 
+
+The collection of all payment sets is in
+
+a one to one correspondents with a strictly decreasing sequence gotten by ordering the set. This algorithm counts ordered sets.
+
+The initial conditions are quite natural and are needed for the recursive process. 
+
+The code works by first asking (for x>0) "is the largest coin in the sequence?", where it's weight is y. 
+
+If yes, it suffices to count the descending sequences for x-y with the same coin list (i.e assume we already paid y).
+
+If no, then throw that coin out of the list and restart the process with the new list of coins.
+
+As this is a clear perfect partition, we can guarantee the accuracy of our results. 
+
 """
 
-print(how_to_split_a(25,[1,5,10,25,100]))
+print(how_to_split_a(200,[1,2,5,10,20,50,100,200]))
+
